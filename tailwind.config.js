9f602bac006d9d065c9ec0935ab575826a875aff
Yen13Sky <Yen13Sky@gmail.com>
2024-08-07 14:31:17 +0300

    ADWIN-test

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	content: ["./index.html"],
+	theme: {
+		extend: {},
+		fontFamily: {
+			inter: ["inter", "sans-serif"]
+		},
+		colors: {
+			'grey': '#D1D5DB',
+			'border-btn': '#E5E7EB',
+			'white': '#FFFFFF',
+			'grey-font': '#6B7280',
+			'header-color': '#1F2937',
+			'black': '#021526',
+
+		}
+	},
+	plugins: [],
+}
